065d3c9ee4871a8bd624936fb4a90127c212c29e
Ibrahim Gidi <114783401+BlackLionXD@users.noreply.github.com>
2024-04-30 03:30:10 +0300

    registration-copied-not_working

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),  # This maps the URL '/djangoapp/logout' to the logout_request view
+    path('register/', views.registration, name='register'),
     # path for dealer reviews view
 
     # path for add a review view
